7d4dcf9558ec3a23e968b20a6c871f9808c544c2
Ninder Joshi <joshi.ninder08@gmail.com>
2020-06-04 16:03:37 +0530

    Files Added

---
+++ Project 1 explore weather trends/query.sql
@@ -0,0 +1,8 @@
+SELECT city_data.year, 
+       city_data.avg_temp   "City Avgerage", 
+       global_data.avg_temp "Global Average" 
+FROM   city_data 
+       INNER JOIN global_data 
+               ON city_data.year = global_data.year 
+WHERE  city_data.city = 'Ludhiana' 
+       AND city_data.avg_temp IS NOT NULL 
